fe5aedaacb32827f8aed7392c65fdbeb2245403c
shenzhanTh <2376047547@qq.com>
2024-11-12 20:01:51 +0800

    添加test_lexer.py

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,8 +1,10 @@
-from tokensAndAnalysis import Lexer
-from nfa import NFAGenerator
-from dfa import nfa_to_dfa
-from minimize_dfa import MinimizedDFA
+# test.py
+from src.Lexer.dfa import nfa_to_dfa
+from src.Lexer.lextest import MinimizedDFA
+from src.Lexer.nfa import NFAGenerator
+from src.Lexer.tokensAndAnalysis import Lexer
 
+# 测试代码
 code = "int main() { return 0; }"
 lexer = Lexer()
 tokens = lexer.tokenize(code)
@@ -11,16 +13,19 @@ print("Tokens:")
 for token in tokens:
     print(f"{token[1]}: {token[0]}")
 
+# 构建 NFA
 nfa_generator = NFAGenerator(tokens)
 start_state = nfa_generator.build_nfa()
 
 print("\nNFA Transitions:")
 nfa_generator.display_nfa(start_state)
 
+# 转换为 DFA
 dfa = nfa_to_dfa(start_state)
 print("\nDFA Transitions:")
 dfa.display_dfa()
 
+# 最小化 DFA
 minimized_dfa = MinimizedDFA(dfa)
 minimized_dfa.minimize()
 print("\nMinimized DFA Transitions:")
