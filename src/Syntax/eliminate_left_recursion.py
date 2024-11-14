a39aa46f99ad43f3aa1d008d769fc8167504e8b9
Balance4 <18722502597@163.com>
2024-11-14 09:06:19 +0800

    添加消除左递归对新增文法的测试

--- src/Syntax/eliminate_left_recursion.py
+++ src/Syntax/eliminate_left_recursion.py
@@ -33,7 +33,7 @@ class LeftRecursionEliminator:
         return non_terminal_symbols, list(terminal_symbols)
 
     # 消除左递归
-    def eliminate_left_recursion(self, non_terminal_symbols, grammar):
+    def eliminate_left_recursion(self, non_terminal_symbols:list, grammar:Grammar):
         """
 
         :param non_terminal_symbols: 非终结符列表
@@ -47,10 +47,8 @@ class LeftRecursionEliminator:
                 i_rules = grammar.rules[i_mark]
                 j_rules = grammar.rules[j_mark]
                 grammar.rules[i_mark] = self.__expand_grammar(i_mark, i_rules, j_mark, j_rules)
-            # print(self.new_productions.rules)
             self.__eliminate_direct_left_recursion_for_one_rule(i_mark, grammar.rules[i_mark])
         self.__remove_useless_productions(non_terminal_symbols, grammar)
-        # print(self.new_productions)
         return grammar
 
     # 实现规则改写
@@ -72,8 +70,6 @@ class LeftRecursionEliminator:
                     new_rules.add(tuple(new_rule))
             else:
                 new_rules.add(tuple(target_rule))
-        # print(new_rules)
-        # print('---------------')
         res_list = [list(r) for r in new_rules]
 
         return res_list
@@ -87,10 +83,8 @@ class LeftRecursionEliminator:
                 left_recursive.append(rhs[1:])
             else:
                 non_left_recursive.append(rhs)
-
         if not left_recursive:
             return rhs_rules
-
         new_lhs = f"{lhs}'"
         # 将非左递归产生式添加新的非终结符
         for nlr in non_left_recursive:
@@ -99,7 +93,6 @@ class LeftRecursionEliminator:
         for lr in left_recursive:
             lr.append(new_lhs)
         left_recursive.append(['$'])
-
         self.new_productions.rules[lhs] = non_left_recursive
         self.new_productions.rules[new_lhs] = left_recursive
 
@@ -129,8 +122,10 @@ class LeftRecursionEliminator:
 if __name__ == "__main__":
     test_path = "../../input/test_grammars.txt"
     path = "../../input/grammars.txt"
+    ext_path = "../../input/extended_grammars.txt"
     output_path = "../../output/grammar_without_left_recursion.txt"
-    oldGrammar = load_from_file(path)
+
+    oldGrammar = load_from_file(ext_path)
     newGrammar = LeftRecursionEliminator(oldGrammar).new_productions
     newGrammar.output_grammar(output_path)
     # print(newGrammar)
