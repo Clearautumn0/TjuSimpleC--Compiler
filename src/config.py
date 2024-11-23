24043f7a8de9e0013f122b94cb3a9e04687f488e
Niezhehao <3022244426@tju.edu.cn>
2024-11-23 15:36:01 +0800

    语法错误时输出规约序列，标记错误符号

--- src/config.py
+++ src/config.py
@@ -3,10 +3,10 @@ GRAMMAR_INPUT_DIR = "../input/extended_grammars.txt"
 
 
 
-TEST_CODE_DIR = "../input/lex_input/test_base_1.c"
-TEST_ID= "1"
+TEST_CODE_DIR = "../input/lex_input/test_switch.c"
+TEST_ID= "_switch"
 PARSER_OUTPUT_DIR = "../output/parser_output"
 
 
 LEX_OUTPUT_FILE = f"{LEX_OUTPUT_DIR}/lex{TEST_ID}.txt"
-PARSER_OUTPUT_FILE = f"{PARSER_OUTPUT_DIR}/lex{TEST_ID}.txt"
+PARSER_OUTPUT_FILE = f"{PARSER_OUTPUT_DIR}/syntax{TEST_ID}.txt"
